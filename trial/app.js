0fdd5f807cab7e916b8ae343e8712ad108cc48bd
TrafalgarLaw28 <roshusaj2014@gmail.com>
2020-02-23 22:37:20 +0530

    app.js added

---
+++ trial/app.js
@@ -0,0 +1,13 @@
+// t = 0;
+
+// var timer = setInterval(function(){
+//     t = t+2;
+//     console.log(t+" seconds have passed");
+//     if (t>5) 
+//     {
+//         clearInterval(timer);
+//     }
+// },2000);
+// console.log("yooo");
+// console.log(__dirname); 
+// console.log(__filename);
